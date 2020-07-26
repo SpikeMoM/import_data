4998e9ba7addd976c0a2c72b4d636c6849550ec8
Spike <wuwenjiee2018@163.com>
2020-07-26 19:39:23 +0800

    add one method to import CST data

--- import_data.m
+++ import_data.m
@@ -1,9 +1,22 @@
 %% CST数据导入
+% method 1
 point = 1003;
 data_row = 5; %sweep的数量*参量数（S11、S21...），参数纵向排列
 data_col = 2; %一般为2：频率、参量，天线方向图为多个参量
 for i = 1:data_row
-    CST = dlmread('cst_test.txt','',[(i-1)*(point+3)+2 0 (i)*(point+3)-2 data_col-1]);
+    CST1(1+(i-1)*point:i*point, :) = dlmread('cst_test.txt','',[(i-1)*(point+3)+2 0 (i)*(point+3)-2 data_col-1]);
+end
+% method 2
+num = 8;
+CST2 = zeros(point, num+1);
+for i = 1:8
+    if i == 1
+        %读取频率+第一列参数
+        CST2(1:point, 1:2) = dlmread('Real.txt','',[(i-1)*(point+3)+2 0 (i)*(point+3)-2 1]);
+    else
+        %第一列之后的参数
+        CST2(1:point, i+1) = dlmread('Real.txt','',[(i-1)*(point+3)+2 1 (i)*(point+3)-2 1]);
+    end
 end
 %% HFSS数据导入
 point = 241;
